7350c8abed5d70e34e5df9ff27503b6302d3aca0
Nui Suriya <atchara.suriya@gmail.com>
2022-11-30 14:50:38 +0100

    fixed bug

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -23,11 +23,12 @@ class ActivitiesController < ApplicationController
         info_window: render_to_string(partial: "info_window", locals: {activity: activity}),
         image_url: helpers.asset_url("logo.png")
       }
-
+    end
     if params[:query].present?
       @activities = Activity.where("name ILIKE ?", "%#{params[:query]}%")
     else
       @activities = Activity.all
     end
   end
+
 end
